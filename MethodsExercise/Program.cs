329774d3820599860fda33ce5b6833db1933e5d0
MuscuarBean <email@example.com>
2023-07-25 10:43:27 -0700

    Created methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,55 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+
+        public static int sum (int num1, int num2)
         {
-            Console.WriteLine("Hello, World!");
+            return num1 + num2;
+        }
+
+        public static int multiply (int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int divide (int num1, int num2)
+        {
+            return num1 / num2;
+        }
+         public static int subtract (int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
+        static void Main(string[] args)
+        
+
+        {
+            int workHours = sum(8, 12);
+
+            int plain = multiply(22, 3);
+
+            int Blep = divide(8, 2);
+
+            int sub = subtract(20, 12);
+
+
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+           
+            Console.WriteLine("What is your favorite color?");
+            string favColor = Console.ReadLine();
+            
+            Console.WriteLine("What is your favorite animal?");
+            string favAnimal = Console.ReadLine();
+            
+            Console.WriteLine("And finally, what is your favorite band?");
+            string favBand = Console.ReadLine();
+            
+            Console.WriteLine($"This future coders name is {userName}, his favorite color is {favColor}, his favorite animal is a {favAnimal}, and lastly, his favorite band is {favBand}");
+
+
         }
     }
 }
